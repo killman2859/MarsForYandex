653b310cb88c19dbd038e3f32a2c95b1f65c6184
killman2859 <78819909+killman2859@users.noreply.github.com>
2025-02-20 19:03:18 +0300

    Task 3

--- server.py
+++ server.py
@@ -18,5 +18,10 @@ def promotion():
     return "Человечество вырастает из детства.<br>Человечеству мала одна планета.<br>Мы сделаем обитаемыми безжизненные пока планеты.<br>И начнем с Марса!<br>Присоединяйся!"
 
 
+@app.route('/image_mars')
+def image_mars():
+    return '''<html><title>Привет, Марс!</title><h1>Жди нас, Марс!</h1><img src="static/Images/mars.png" alt="Mars surface" width=500 height=500><br><br>Вот она красная планета!</html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
