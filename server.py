7b4096a6cb8f9fc67ee31325cb804b282f323d0b
killman2859 <78819909+killman2859@users.noreply.github.com>
2025-02-20 18:52:41 +0300

    Task 2

--- server.py
+++ server.py
@@ -6,9 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def start():
     return "Миссия Колонизация Марса"
+
+
 @app.route('/index')
 def index():
     return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    return "Человечество вырастает из детства.<br>Человечеству мала одна планета.<br>Мы сделаем обитаемыми безжизненные пока планеты.<br>И начнем с Марса!<br>Присоединяйся!"
 
 
 if __name__ == '__main__':
